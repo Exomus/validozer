b6f31b8d57e90daa9a0acd7674a0c12514005534
Adam NAILI <a.naili@live.fr>
2021-01-28 16:37:53 +0100

    Remove dist but add a way to prepare npm install from a github

--- dist/error/validation.error.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class ValidationError extends Error {
-    /**
-     * Constructs an IllegalArgumentError with an optional detail message.
-     */
-    constructor(errorList: Error[]);
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './monad/validator';
-export * from './error/illegal-argument.error';
-export * from './error/illegal-state.error';
