f10b00fc815d1b71ba0c0d4c21db6172d2c5f2e9
Adam NAILI <a.naili@live.fr>
2021-01-28 16:56:59 +0100

    Fix broken message in the validation error

--- lib/error/validation.error.ts
+++ lib/error/validation.error.ts
@@ -9,16 +9,15 @@ export class ValidationError extends Error {
      * Constructs an IllegalArgumentError with an optional detail message.
      */
     constructor(errorList: Error[]) {
-        super();
         if (!errorList) {
             throw new IllegalArgumentError('errorList must be defined');
         }
-        this.name = ValidationError.name;
 
         let builtInMessage = 'One or multiple errors have been found during validation:\n'
         errorList.forEach((error, index) => {
-            builtInMessage += error.message ? `${index + 1}) ${error.name}\n` : `${index + 1}) ${error.name} - ${error.message}\n`;
+            builtInMessage += !error.message ? `${index + 1}) ${error.name}\n` : `${index + 1}) ${error.name} - ${error.message}\n`;
         })
-        this.message = builtInMessage;
+        super(builtInMessage);
+        this.name = ValidationError.name;
     }
 }
